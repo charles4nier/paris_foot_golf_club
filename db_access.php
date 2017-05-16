c90efc382d5c5a4d87859bb39989490a8cfea861
BucKz96 <lefevre.maxime96@gmail.com>
2017-05-16 17:25:24 +0200

    Ajout script connect DB

---
+++ db_access.php
@@ -0,0 +1,16 @@
+<?php
+
+$user = 'phpmyadmin';
+$pwd = 'max';
+
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=footgolf;charset=utf8', $user, $pwd, array(PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION));
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+
+
+
+?> 
