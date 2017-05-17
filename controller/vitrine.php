6a51c7633f8f527034c7b86acec2d63a88bc8b95
charles4nier <charles.4nier@gmail.com>
2017-05-17 14:25:16 +0200

    Avec architecture mvc

---
+++ controller/vitrine.php
@@ -0,0 +1,16 @@
+	<?php
+
+	require 'db_access.php';
+
+	$links = $bdd->query('SELECT * FROM upload');
+	?>
+
+
+	<section class="container">
+		<div class="row">
+			<?php foreach ($links as $link) { ?>
+			<div class="card col-xs-4 col-md-4">
+				<p class="col-xs-offset-1 col-xs-4 col-md-4" id="id_image"> Id : <?php echo $link['id']; ?></p>
+				<?php echo '<img class="card-img vitrine" src="data:'.utf8_encode($link['type']).';base64,'.base64_encode(stripslashes($link['content'])). '"/>';?>
+			</div>
+			<?php } ?> 
