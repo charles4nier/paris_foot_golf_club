1fcdadc7076325500a871ef9f9fe8f9cc87793c9
BucKz96 <lefevre.maxime96@gmail.com>
2017-05-23 10:16:51 +0200

    pull

--- controller/signup.php
+++ controller/signup.php
@@ -1,16 +1,4 @@
 <?php
-	var_dump($_POST['pack']);
-	var_dump($_POST['prenom']);
-	var_dump($_POST['nom']);
-	var_dump($_POST['age']);
-	var_dump($_POST['sexe']);
-	var_dump($_POST['cm?']);
-	var_dump($_POST['ville']);
-	var_dump($_POST['adresse']);
-	var_dump($_POST['telephone']);
-	var_dump($_POST['mail']);
-	var_dump($_POST['pass']);
-
 
 if (
 	
@@ -29,23 +17,40 @@ isset($_POST['pass'])
 
 ) {
 
-	echo "bonjour";
+	echo $_POST['mail'];
 
-// 	$pack = $_POST['pack'];
-// 	$prenom = $_POST['prenom'];
-// 	$nom = $_POST['nom']; 
-// 	$age = $_POST['age']; 
-// 	$sexe = $_POST['sexe']; 
-// 	$cm = $_POST['cm?']; 
-// 	$ville = $_POST['ville']; 
-// 	$adresse = $_POST['adresse']; 
-// 	$telephone = $_POST['telephone']; 
-// 	$mail = $_POST['mail']; 
-// 	$pass_hache = sha1($_POST['pass']);
+	$pack = $_POST['pack'];
+	$prenom = $_POST['prenom'];
+	$nom = $_POST['nom']; 
+	$age = $_POST['age']; 
+	$sexe = $_POST['sexe']; 
+	$cm = $_POST['cm?']; 
+	$ville = $_POST['ville']; 
+	$adresse = $_POST['adresse']; 
+	$telephone = $_POST['telephone']; 
+	$mail = $_POST['mail']; 
+	$pass_hache = sha1($_POST['pass']);
 
 
 
-// require '../modele/db_access.php';
+require '../modele/db_access.php';
+
+
+$req = $bdd->prepare('INSERT INTO users(firstname, lastname, pack, mail, pass, date, age, sexe, cm, ville, adresse, phone) VALUES(:firstname, :lastname, :pack, :mail, :pass, CURDATE(), :age, :sexe, :cm, :ville, :adresse, :phone)');
+	$req->execute(array(
+		'firstname' => $firstname,
+		'lastname' => $lastname,
+		'pack' => $pack,
+		'mail' => $mail,
+		'pass' => $pass_hache,
+		'age' => $age,
+		'sexe' => $sexe,
+		'cm' => $cm,
+		'ville' => $ville,
+		'adresse' => $adresse,
+		'phone' => $phone
+		));
+	echo "upload effectué";
 
 
 // $content = $bdd->prepare('INSERT INTO users(firstname, lastname, pack, mail, pass, date, age, sexe, cm, ville, adresse, phone) VALUES(:firstname, :lastname, :pack, :mail, :pass, CURDATE(), :age, :sexe, :cm, :ville, :adresse, :phone)'); 
@@ -63,7 +68,7 @@ isset($_POST['pass'])
 // 	'phone' => $phone
 //  	)); 
 
-
+header("Location: http://localhost/footgolf/paris_foot_golf_club/view/maquette.php");
 
 // $content->closeCursor(); 
 }
